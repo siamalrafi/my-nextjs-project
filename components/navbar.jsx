bbc7d53801f99dc61f0893835d459ca592f1dba4
siamalrafi <rahathasandipto@gmail.com>
2023-04-13 17:06:43 +0600

    about page

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,28 +1,56 @@
+import Link from "next/link";
+
+
 function navbar() {
     return (
         <div>
             <div className="navbar bg-base-100">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+
+                    <Link href="/" className="btn btn-ghost normal-case text-xl">
+                        NEXT^JS
+                    </Link>
+
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
+                        <li>
+                            <Link href="/about" passHref>
+                                About Us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/services" passHref>
+                                Services
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/blog" passHref>
+                                Blog
+                            </Link>
+                        </li>
                         <li tabIndex={0}>
                             <a>
-                                Parent
+                                Account
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                             </a>
                             <ul className="p-2 bg-base-100">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li>
+                                    <Link href="/register" passHref>
+                                        Register
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/login" passHref>
+                                        Login
+                                    </Link>
+                                </li>
                             </ul>
                         </li>
-                        <li><a>Item 3</a></li>
                     </ul>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 

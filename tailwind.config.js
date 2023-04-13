c0e45fc2f783511b05408aa38aeb161959598436
siamalrafi <rahathasandipto@gmail.com>
2023-04-13 16:41:09 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,8 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  daisyui: {
+    themes: false,
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
